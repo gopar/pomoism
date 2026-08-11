4c302b54d37f5f2f0477ce9bdd9bcd9350969a31
Gopar <gopardaniel@gmail.com>
2026-08-11 10:46:44 -0700

    Fix CI config

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ from pathlib import Path
 import pytest
 
 REPO_ROOT = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(REPO_ROOT))
+sys.path.insert(0, str(REPO_ROOT / "src"))
 
 from pomo import common  # noqa: E402
 
